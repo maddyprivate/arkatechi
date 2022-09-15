0fe1080f115f55c075f4b6f979b8753183ad2509
Hitesh <patilhitesh16@gmail.com>
2022-09-15 15:23:18 +0530

    dropdown

---
+++ clang.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<?php include('head.php'); ?>
+<body>
+    <?php include('header.php'); ?>
+    <main>
+        <!-- ============abt-01 Section  Start============ -->        
+        <section class="abt-01">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading-wrapper">
+                            <h3>COURSE DETAILS</h3>
+                            <ol>
+                                <li>C LANGUAGE<i class="far fa-angle-double-right"></i></li>
+                                <li>C LANGUAGE</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ============bg-se-02  Section  Start============ -->
+        <section class="bg-se-02">
+            <div class="container">
+            <br>Basic concepts</br>
+            <br>Functions </br>
+            <br>Declaration & Prototype</br>
+            <br>Passing & returning value </br>
+            <br>Passing array to function & Element to Funtion </br>
+            <br>Recursive </br>
+            <br>Pointers (Concept, pointer arrays & Character array) </br>
+            <br>Structures ( Declaring, Accessing, Array of, Pointer of ) </br>
+            <br>File Handling (File operation, Different modes of files)</br>
+            <br>Files (command Lines, arguments, Misc. I/O function, mode of files)</br>
+            </div>
+        </section>
+    </main>
+    <!-- ============Footer  Start============ -->
+    <?php include('footer.php'); ?>
+</body>
+
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/plugins/owl.carousel.js"></script>
+<script src="assets/js/plugins/owl.carousel.min.js"></script>
+<script src="assets/js/script.js"></script>
+
+</html>
